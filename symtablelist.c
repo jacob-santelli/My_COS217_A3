56944ee4cf8cd243ff9542d5d97c8f8454b83761
santelli <santelli@princeton.edu>
2022-10-24 17:38:40 -0400

    bruh

--- symtablelist.c
+++ symtablelist.c
@@ -1,15 +1,51 @@
+#include <assert.h>
+#include <stdlib.h>
+#include "symtable.h"
+
+struct SymTableNode
+{
+   /* The item and key. */
+   const char *pckey;
+   const void *pvItem;
+
+   /* The address of the next StackNode. */
+   struct SymTableNode *psNextNode;
+};
+
+struct SymTable
+{
+   /* The address of the first StackNode. */
+   struct SymTableNode *psFirstNode;
+};
+
 /* SymTable_new must return a new SymTable object that contains no bindings, or NULL if insufficient memory is available. */
 SymTable_T SymTable_new(void) {
-    
+   SymTable_T oSymTable;
+
+   oSymTable = (SymTable_T)malloc(sizeof(struct SymTable));
+   if (oSymTable == NULL) return NULL;
+
+   oSymTable->psFirstNode = NULL;
+   return oSymTable;
 }
 
 /* SymTable_free must free all memory occupied by oSymTable. */
 void SymTable_free(SymTable_T oSymTable) {
-
+   assert(oSymTable != NULL);
+   free(oSymTable);
 }
 
 /* SymTable_getLength must return the number of bindings in oSymTable. */
 size_t SymTable_getLength(SymTable_T oSymTable) {
+   const char *temp;
+   size_t length;
+   assert(oSymTable != NULL);
+
+   temp = oSymTable->psFirstNode;
+   while (*temp != NULL) {
+      length++;
+      
+   }
 
 }
 
@@ -35,35 +71,45 @@ not even be in the heap! Freeing it could be a disaster!) */
 
 
 int SymTable_put(SymTable_T oSymTable, const char *pcKey, const void *pvValue) {
-
+   assert(oSymTable != NULL);
+   assert(pcKey != NULL);
+   assert(pvValue != NULL);
 }
 
 /* If oSymTable contains a binding with key pcKey, then SymTable_replace must replace the binding's 
 value with pvValue and return the old value. Otherwise it must leave oSymTable unchanged and return NULL. */
 void *SymTable_replace(SymTable_T oSymTable, const char *pcKey, const void *pvValue) {
-
+   assert(oSymTable != NULL);
+   assert(pcKey != NULL);
+   assert(pvValue != NULL);   
 }
 
 /* SymTable_contains must return 1 (TRUE) if oSymTable contains a binding whose key is pcKey, and 0 (FALSE) otherwise. */
 int SymTable_contains(SymTable_T oSymTable, const char *pcKey) {
-
+   assert(oSymTable != NULL);
+   assert(pcKey != NULL);
 }
 
 
 /* SymTable_get must return the value of the binding within oSymTable whose key is pcKey, or NULL if no such binding exists.*/
 
 void *SymTable_get(SymTable_T oSymTable, const char *pcKey) {
-
+   assert(oSymTable != NULL);
+   assert(pcKey != NULL);
 }
 
 /* If oSymTable contains a binding with key pcKey, then SymTable_remove must remove that binding from oSymTable and 
 return the binding's value. Otherwise the function must not change oSymTable and return NULL. */
 void *SymTable_remove(SymTable_T oSymTable, const char *pcKey) {
-
+   assert(oSymTable != NULL);
+   assert(pcKey != NULL);
 }
 
 /* SymTable_map must apply function *pfApply to each binding in oSymTable, passing pvExtra as an extra parameter. 
 That is, the function must call (*pfApply)(pcKey, pvValue, pvExtra) for each pcKey/pvValue binding in oSymTable. */
 void SymTable_map(SymTable_T oSymTable, void (*pfApply)(const char *pcKey, void *pvValue, void *pvExtra), const void *pvExtra) {
+   assert(oSymTable != NULL);
+   assert(pfApply != NULL);
+   assert(pvExtra != NULL); 
 
 }
